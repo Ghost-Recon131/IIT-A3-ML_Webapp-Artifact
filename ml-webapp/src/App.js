8240f901a2460441e6596aa39126313cbed139fa
Jingxuan Feng <henryfeng24@pm.me>
2022-05-28 15:31:54 +1000

    initial commit of React code

--- ml-webapp/src/App.js
+++ ml-webapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is starting code for our Webapp artifact
         </p>
         <a
           className="App-link"
